7189dee4f7bb85e42c060f7ffab97508e031c04c
wpjscc <wpjscc@gmail.com>
2024-08-06 13:32:07 +0800

    facade

---
+++ src/Facades/ReactServer.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Reactphp\Framework\LaravelReactphp\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * @method void static get(string $route, $handler, ...$handlers)
+ * @method void static post(string $route, $handler, ...$handlers)
+ * @method void static put(string $route, $handler, ...$handlers)
+ * @method void static delete(string $route, $handler, ...$handlers)
+ * @method void static patch(string $route, $handler, ...$handlers)
+ * @method void static options(string $route, $handler, ...$handlers)
+ * @method void static head(string $route, $handler, ...$handlers)
+ * @method void static any(string $route, $handler, ...$handlers)
+ * @method void static addGroup(string $prefix, array $handlers, callable $callback)
+ * @method void static map(array $methods, string $route, $handler, ...$handlers)
+ */
+
+class ReactServer extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'reactphp.server';
+    }
+}
